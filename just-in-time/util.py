a32e43ff4a75d9a1f56ff2e526a931a818112760
monilouise <moniquelouise@gmail.com>
2025-05-06 21:12:57 -0300

    - New arguments eval_metric, do_resume_training, oversample, undersample, online_mode, calculate_metrics, skewed_oversample, window_size, target_th, l0, l1, m, only_manual

--- just-in-time/util.py
+++ just-in-time/util.py
@@ -3,7 +3,9 @@ import random
 import numpy as np
 import torch
 from transformers import (RobertaModel, RobertaTokenizer, RobertaConfig, T5ForConditionalGeneration, T5Config,
-                          PLBartTokenizer, PLBartForConditionalGeneration, PLBartConfig)
+                          PLBartTokenizer, PLBartForConditionalGeneration, PLBartConfig, CodeGenTokenizer, 
+                          AutoTokenizer, AutoModel)
+import logging
 
 
 def parse_jit_args():
@@ -39,17 +41,32 @@ def parse_jit_args():
     parser.add_argument("--do_test", action='store_true')
     parser.add_argument("--use_lora", action='store_true')
 
+    parser.add_argument("--eval_metric", type=str, default="f1")
+    parser.add_argument("--do_resume_training", action='store_true', default=False)
+    parser.add_argument("--oversample", action='store_true', default=False)
+    parser.add_argument("--undersample", action='store_true', default=False)
+    parser.add_argument("--online_mode", action='store_true', default=False)
+    parser.add_argument("--calculate_metrics", action='store_true', default=False)
+    
+    parser.add_argument("--skewed_oversample", action='store_true', default=False)
+    parser.add_argument("--window_size", type=int, default=100)
+    parser.add_argument("--target_th", type=float, default=0.5)
+    parser.add_argument("--l0", type=float, default=10)
+    parser.add_argument("--l1", type=float, default=12)
+    parser.add_argument("--m", type=float, default=1.5)
+    parser.add_argument("--only_manual", action='store_true', default=False)
+
     args = parser.parse_args()
     return args
 
 
 def set_seed(args):
+    print("Seed = ", args.seed)
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     if args.n_gpu > 0:
         torch.cuda.manual_seed_all(args.seed)
-
 
 def build_model_tokenizer_config(args):
     model_classes = {
@@ -58,25 +75,50 @@ def build_model_tokenizer_config(args):
         "codet5": (T5ForConditionalGeneration, RobertaTokenizer, T5Config, "Salesforce/codet5-base"),
         "unixcoder": (RobertaModel, RobertaTokenizer, RobertaConfig, "microsoft/unixcoder-base"),
         "plbart": (PLBartForConditionalGeneration, PLBartTokenizer, PLBartConfig, "uclanlp/plbart-base"),
-        "plbart-large": (PLBartForConditionalGeneration, PLBartTokenizer, PLBartConfig, "uclanlp/plbart-large")
+        "plbart-large": (PLBartForConditionalGeneration, PLBartTokenizer, PLBartConfig, "uclanlp/plbart-large"),
+        #fork by monilouise
+        "codet5p": (T5ForConditionalGeneration, RobertaTokenizer, T5Config, "Salesforce/codet5p-220m-bimodal"),
+        "codet5p-770m": (T5ForConditionalGeneration, RobertaTokenizer, T5Config, "Salesforce/codet5p-770m"),
+        "codet5p-2b": (T5ForConditionalGeneration, CodeGenTokenizer, T5Config, "Salesforce/codet5p-2b"),
+        "codet5p-6b": (T5ForConditionalGeneration, CodeGenTokenizer, T5Config, "Salesforce/codet5p-6b"),
+        "codet5p-16b": (T5ForConditionalGeneration, CodeGenTokenizer, T5Config, "Salesforce/codet5p-16b"),
+        "codereviewer": (T5ForConditionalGeneration, RobertaTokenizer, T5Config, "microsoft/codereviewer"),
+        "modernbert": (AutoModel, AutoTokenizer, None, "answerdotai/ModernBERT-base"),
+        "modernbert-large": (AutoModel, AutoTokenizer, None, "answerdotai/ModernBERT-large"),
     }
 
     model_class, tokenizer_class, config_class, actual_name = model_classes[args.pretrained_model]
 
     # load config.
-    config = config_class.from_pretrained(actual_name)
-    if args.pretrained_model in ["codebert", "graphcodebert", "unixcoder"]:
-        config.hidden_size = args.hidden_size
-    elif args.pretrained_model in ["codet5", "plbart", "plbart-large"]:
-        config.d_model = args.hidden_size
-    config.hidden_dropout_prob = args.dropout
-    config.attention_probs_dropout_prob = args.dropout
+    config = None
+    if config_class:
+        config = config_class.from_pretrained(actual_name)
+        if args.pretrained_model in ["codebert", "graphcodebert", "unixcoder"]:
+            config.hidden_size = args.hidden_size
+        elif args.pretrained_model in ["codet5", "plbart", "plbart-large"]:
+            config.d_model = args.hidden_size
+
+        config.hidden_dropout_prob = args.dropout
+        config.attention_probs_dropout_prob = args.dropout
+    
     # load tokenizer.
     tokenizer = tokenizer_class.from_pretrained(actual_name)
     special_tokens_dict = {"additional_special_tokens": ["[ADD]", "[DEL]"]}
+
+    if args.pretrained_model in ["codet5p-2b", "codet5p-6b", "codet5p-16b"]:
+        special_tokens_dict["additional_special_tokens"].extend(["<s>", "</s>"])
+        tokenizer.cls_token = "<s>"
+        tokenizer.sep_token = "</s>"
+
     tokenizer.add_special_tokens(special_tokens_dict)
     # load pretrained model.
-    model = model_class.from_pretrained(actual_name, config=config)
+    if config:
+        model = model_class.from_pretrained(actual_name, config=config)
+    else:
+        logger = logging.getLogger(__name__)
+        logger.info(f"Loading model {actual_name}")
+        model = model_class.from_pretrained(actual_name)
+
     model.resize_token_embeddings(len(tokenizer))
 
     return model, tokenizer, config
