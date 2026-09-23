c86785be6709b3ec9927890aef6dd6aa304af4bd
monilouise <moniquelouise@gmail.com>
2026-09-23 15:19:22 -0300

    refactoring

--- just-in-time/models/ConcatModel.py
+++ just-in-time/models/ConcatModel.py
@@ -40,25 +40,32 @@ class ConcatModel(nn.Module):
         self._attn_success = 0
         self._attn_fail = 0
 
-    def forward(self, input_ids, input_mask, manual_features, label=None, output_attentions=None):
+    def forward(self, input_ids, input_mask, manual_features, label=None, output_attentions=None, attn_implementation=None):
         if self.args.pretrained_model in ["codebert", "graphcodebert", "unixcoder", "modernbert", "modernbert-large"]:
-            if self.args.use_lora:
-                outputs = self.encoder.base_model.model(input_ids=input_ids, attention_mask=input_mask, output_attentions=output_attentions)
+            if self.args.use_lora and not self.args.do_predict:
+                outputs = self.encoder.base_model.model(input_ids=input_ids, attention_mask=input_mask, 
+                                                        output_attentions=output_attentions, 
+                                                        attn_implementation=attn_implementation)
+            elif self.args.use_lora and self.args.do_predict: 
+                outputs = self.encoder(input_ids=input_ids, attention_mask=input_mask, 
+                                       output_attentions=output_attentions, attn_implementation=attn_implementation)
             else:
                 outputs = self.encoder(input_ids=input_ids, attention_mask=input_mask, 
-                                       output_attentions=output_attentions)
+                                       output_attentions=output_attentions, attn_implementation=attn_implementation)
         elif self.args.pretrained_model in ["codet5", "codet5p", "codet5p-770m", "codet5p-2b", 
                                             "codet5p-6b", "codet5p-16b", "codereviewer"]:
             outputs = self.encoder.encoder(input_ids=input_ids, attention_mask=input_mask, 
-                                           output_attentions=output_attentions)
+                                           output_attentions=output_attentions, attn_implementation=attn_implementation)
         elif self.args.pretrained_model in ["plbart", "plbart-large"]:
             if self.args.use_lora:
                 outputs = self.encoder.base_model.model.model.encoder(input_ids=input_ids, 
                                                                       attention_mask=input_mask, 
-                                                                      output_attentions=output_attentions)
+                                                                      output_attentions=output_attentions,
+                                                                      attn_implementation=attn_implementation)
             else:
                 outputs = self.encoder.model.encoder(input_ids=input_ids, attention_mask=input_mask, 
-                                                     output_attentions=output_attentions)
+                                                     output_attentions=output_attentions, 
+                                                     attn_implementation=attn_implementation)
 
         logits = self.classifier(outputs[0], manual_features)
 
@@ -117,7 +124,7 @@ class ConcatModel(nn.Module):
                         logging.getLogger(__name__).warning(
                             f"Attention extraction failures: {self._attn_fail} / "
                             f"{self._attn_fail + self._attn_success} batches")
-            return prob, loss, last_layer_attn_weights
+            return prob, loss, last_layer_attn_weights 
         else:
             if output_attentions:
                 if last_layer_attn_weights is not None:
@@ -163,4 +170,188 @@ class Attention(nn.Module):       #x:[batch, seq_len, hidden_dim*2]
             return context
         else:
             return scored_x
+
+class HAN_MODEL(nn.Module):
+    def __init__(self, embedding_layer):
+        super().__init__()
+        self.hidden_size = 256
+        self.num_layers = 1
+        self.bidirectional = True
+        self.embedding = embedding_layer
+
+        self.lstm1 = nn.LSTM(input_size=self.embedding.embedding_dim,
+                            hidden_size=self.hidden_size,
+                            num_layers=self.num_layers,
+                            bidirectional=self.bidirectional,
+                            batch_first=True)
+        self.att1 = Attention(self.hidden_size, need_aggregation=True)
+        self.lstm2 = nn.LSTM(input_size=self.hidden_size*2,
+                            hidden_size=self.hidden_size,
+                            num_layers=self.num_layers,
+                            bidirectional=self.bidirectional,
+                            batch_first=True)
+        self.att2 = Attention(self.hidden_size, need_aggregation=False)
+
+        # self.fc1 = nn.Linear(512, 2)
+        self.fc1 = nn.Linear(512, 128)
+        self.relu = nn.Tanh()
+        self.fc2 = nn.Linear(128, 2)
+        # self.softmax = nn.Softmax(dim=-1)
+        self.dropout = nn.Dropout(0.5)
+
+
+    def forward(self, x):
+        # input x : (bs, nus_sentences, nums_words)
+        device = x.device
+        x = self.embedding(x) # out x : (bs, nus_sentences, nums_words, embedding_dim)
+        x = self.dropout(x)
+        batch_size, num_sentences, num_words, emb_dim = x.shape
+
+        h0_1 = torch.randn(self.num_layers*2, batch_size*num_sentences, self.hidden_size).to(device)
+        c0_1 = torch.randn(self.num_layers*2, batch_size*num_sentences, self.hidden_size).to(device)
+        h0_2 = torch.randn(self.num_layers*2, batch_size, self.hidden_size).to(device)
+        c0_2 = torch.randn(self.num_layers*2, batch_size, self.hidden_size).to(device)
+
+        x = x.view(batch_size*num_sentences, num_words, emb_dim).contiguous()
+        x,(_,_)= self.lstm1(x, (h0_1,c0_1))   # out：batch_size*num_sentences, num_words，hidden_size*2
+        x = self.att1(x)   # batch_size*num_sentences, hidden_size*2
+
+        x = x.view(x.size(0)//num_sentences, num_sentences, self.hidden_size*2).contiguous()
+        x,(_,_)= self.lstm2(x, (h0_2,c0_2))   # out：batch_size, num_sentences，hidden_size*2
+
+        x = self.att2(x)   # batch_size, hidden_size*2
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+
+        return x
     
+class MultiFocalLoss(nn.Module):
+    """
+    Focal_Loss= -1*alpha*((1-pt)**gamma)*log(pt)
+    Args:
+        num_class: number of classes
+        alpha: class balance factor shape=[num_class, ]
+        gamma: hyper-parameter
+        reduction: reduction type
+    """
+
+    def __init__(self, num_class, alpha=None, gamma=2, reduction='mean'):
+        super(MultiFocalLoss, self).__init__()
+        self.num_class = num_class
+        self.gamma = gamma
+        self.reduction = reduction
+        self.smooth = 1e-4
+        self.alpha = alpha
+        if alpha is None:
+            self.alpha = torch.ones(num_class, ) - 0.5
+        elif isinstance(alpha, (int, float)):
+            self.alpha = torch.as_tensor([alpha] * num_class)
+        elif isinstance(alpha, (list, np.ndarray)):
+            self.alpha = torch.as_tensor(alpha)
+        if self.alpha.shape[0] != num_class:
+            raise RuntimeError('the length not equal to number of class')
+
+    def forward(self, logit, target):
+        # assert isinstance(self.alpha,torch.Tensor)\
+        alpha = self.alpha.to(logit.device)
+        prob = F.softmax(logit, dim=1)
+
+        if prob.dim() > 2:
+            # used for 3d-conv:  N,C,d1,d2 -> N,C,m (m=d1*d2*...)
+            N, C = logit.shape[:2]
+            prob = prob.view(N, C, -1)
+            prob = prob.transpose(1, 2).contiguous()  # [N,C,d1*d2..] -> [N,d1*d2..,C]
+            prob = prob.view(-1, prob.size(-1))  # [N,d1*d2..,C]-> [N*d1*d2..,C]
+
+        ori_shp = target.shape
+        target = target.view(-1, 1)
+
+        prob = prob.gather(1, target).view(-1) + self.smooth  # avoid nan
+        logpt = torch.log(prob)
+        # alpha_class = alpha.gather(0, target.squeeze(-1))
+        alpha_weight = alpha[target.squeeze().long()]
+        loss = -alpha_weight * torch.pow(torch.sub(1.0, prob), self.gamma) * logpt
+
+        if self.reduction == 'mean':
+            loss = loss.mean()
+        elif self.reduction == 'none':
+            loss = loss.view(ori_shp)
+
+        return loss
+
+class ConcatModelDL(nn.Module):
+    def __init__(self, encoder, config, tokenizer, args):
+        super(ConcatModelDL, self).__init__()
+        self.encoder = encoder
+        self.config = config
+        self.tokenizer = tokenizer
+        self.args = args
+        self.classifier = RobertaClassifier(args)
+
+        # ----------------------HAN-------------------------------
+        #self.han_word_embedding_layer = self.encoder.embeddings.word_embeddings
+        #self.han_word_embedding_layer = self.encoder.base_model.embeddings.word_embeddings
+        self.han_word_embedding_layer = self.encoder.base_model.get_input_embeddings()
+        self.han_locator = HAN_MODEL(embedding_layer=self.han_word_embedding_layer)
+
+        # --------------------------------------------------------
+
+    def forward(self, input_ids, input_mask, manual_features, label=None, line_ids=None, line_label=None, 
+                output_attentions=None):
+        if self.args.pretrained_model in ["codebert", "graphcodebert", "unixcoder", "modernbert", "modernbert-large"]:
+            if self.args.use_lora:
+                outputs = self.encoder.base_model.model(input_ids=input_ids, attention_mask=input_mask, output_attentions=output_attentions)
+            else:
+                outputs = self.encoder(input_ids=input_ids, attention_mask=input_mask, 
+                                       output_attentions=output_attentions)
+        elif self.args.pretrained_model in ["codet5", "codet5p", "codet5p-770m", "codet5p-2b", 
+                                            "codet5p-6b", "codet5p-16b", "codereviewer"]:
+            outputs = self.encoder.encoder(input_ids=input_ids, attention_mask=input_mask, 
+                                           output_attentions=output_attentions)
+        elif self.args.pretrained_model in ["plbart", "plbart-large"]:
+            if self.args.use_lora:
+                outputs = self.encoder.base_model.model.model.encoder(input_ids=input_ids, 
+                                                                      attention_mask=input_mask, 
+                                                                      output_attentions=output_attentions)
+            else:
+                outputs = self.encoder.model.encoder(input_ids=input_ids, attention_mask=input_mask, 
+                                                     output_attentions=output_attentions)
+
+        last_layer_attn_weights = outputs.attentions[self.config.num_hidden_layers - 1][:, :,
+                                  0].detach() if output_attentions else None 
+        logits = self.classifier(outputs[0], manual_features)
+
+        han_logits = self.han_locator(line_ids)
+        
+        #From JIT-SMART
+        logits = (logits+han_logits.mean(dim=1))/2
+
+        #Original
+        #prob = torch.sigmoid(logits)
+        prob = torch.sigmoid(logits[:, 1])  #prob of positive class
+
+        prob_lines = torch.softmax(han_logits, dim=-1)[:, :, 1]
+
+        if label is not None:
+            loss_fct = nn.BCELoss()
+            #loss1 = loss_fct(prob, torch.unsqueeze(label, dim=1).float())
+            loss1 = loss_fct(prob, label.float())
+
+            loss_fct = nn.BCELoss()
+            
+            #loss2 = loss_fct(prob_lines.view(-1), line_label.reshape((-1,)).float())
+            loss_dl = MultiFocalLoss(alpha=0.25, gamma=2, reduction='mean', num_class=2)
+            loss2 = loss_dl(han_logits.reshape((-1, 2)), line_label.reshape((-1,)))
+            
+            loss = loss1*self.args.dp_loss_weight + loss2*self.args.dl_loss_weight
+
+            return prob, loss, last_layer_attn_weights, prob_lines 
+        else:
+            return prob, prob_lines
+
+    def copy(self):
+        new_model = ConcatModel(self.encoder, self.config, self.tokenizer, self.args)
+        new_model.load_state_dict(self.state_dict())
+        new_model.to(self.args.device)
+        return new_model
